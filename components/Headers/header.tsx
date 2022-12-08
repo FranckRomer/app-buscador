9bdf6dcd2fde8e96269360d29168508124347e40
FranckRomer <paco_metal95@hotmail.com>
2022-12-08 13:49:01 -0600

    buscador home creado

---
+++ components/Headers/header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from "./header.module.css"
+
+const Header = (props: any) => {
+    return (
+        <div className={styles.header}>
+            <Link href={"/"}>
+                {props.tipo == "home" ? <h3>Home</h3> : <h4>Home</h4>}
+            </Link>
+            <Link href={"/diagnostico"}>
+                {props.tipo == "diagnostico" ? <h3>Diagnostico</h3> : <h4>Diagnostico</h4>}
+            </Link>
+            <Link href={"/trainz"}>
+                {props.tipo == "trainz" ? <h3>TrainZ</h3> : <h4>TrainZ</h4>}
+            </Link>
+            {/* <Link href={"/contact"}>
+                {props.tipo == "contact" ? <h3>Contact</h3> : <h4>Contact</h4>}
+            </Link> */}
+        </div>
+    )
+}
+
+export default Header
